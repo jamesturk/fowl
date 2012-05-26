30ccfaab8fc9e1a4a9fa1fdc5d3fc699c30f4dc2
James Turk <james.p.turk@gmail.com>
2012-05-26 19:43:37 -0400

    stables stub

--- fowl/game/urls.py
+++ fowl/game/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^events/$', 'fowl.game.views.events'),
+    url(r'^stables/$', 'fowl.game.views.stables'),
 )
--- fowl/game/views.py
+++ fowl/game/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def events(request):
     return render(request, "events.html")
+
+def stables(request):
+    return render(request, "stables.html")
