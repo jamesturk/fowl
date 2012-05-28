93ca13a63df89de532121bf3e1f837a04ba53c9a
James Turk <james.p.turk@gmail.com>
2012-05-28 13:31:09 -0400

    settings refresh

--- fowl/settings.py
+++ fowl/settings.py
@@ -4,7 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('James Turk', 'james.p.turk@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -16,41 +16,20 @@ DATABASES = {
     }
 }
 
-TIME_ZONE = 'America/New_York'
-
-LANGUAGE_CODE = 'en-us'
-
 SITE_ID = 1
 
+TIME_ZONE = 'America/New_York'
+LANGUAGE_CODE = 'en-us'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = 'collected_static'
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
-
-# Additional locations of static files
 STATICFILES_DIRS = (
     os.path.join(os.path.dirname(__file__), 'bootstrap'),
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
 )
 
 # List of finder classes that know how to find static files in
@@ -61,14 +40,11 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
 SECRET_KEY = 'emixprj(wfq&amp;7ail(!_$!vm%ccd8ydrszt5&amp;%2c7su3mol6+z^'
 
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -83,10 +59,7 @@ MIDDLEWARE_CLASSES = (
 )
 
 INTERNAL_IPS = ('127.0.0.1',)
-
 ROOT_URLCONF = 'fowl.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'fowl.wsgi.application'
 
 TEMPLATE_DIRS = (
